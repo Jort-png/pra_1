e932c134265d6b84b64637a4d41b214eaee2cfe0
Jort-png <Jort@Lievense.nl>
2024-09-15 23:26:50 +0200

    fotos vergroten
    ervoor gezorgt dat de foto van het blok en rooster groter in beeld komen voor betere zichtbaarheid

--- index.php
+++ index.php
@@ -12,22 +12,34 @@ require_once("header.php")
 ?>
 <div class="over">
     <p>Ook kan je als je uitblinkt of al veel kennis hebt van het programmeren kiezen om versneld traject te doen. Dan doe je de opleiding in 3 jaar, daarmee skip je de 2 blokken waaronder 1 blok aan stage</p>
-        <p>De opleiding software developer bij Curio neemt je mee op een reis door de wereld van programmeren en softwareontwikkeling. Je zult zowel web- als native applicaties leren bouwen met talen zoals HTML, CSS, JavaScript, C# en Python. In vier jaar tijd transformeer je van een beginner naar een bekwame softwareontwikkelaar, klaar om te werken in een breed scala van ICT-gerelateerde bedrijven.</p>
+    <p>De opleiding software developer bij Curio neemt je mee op een reis door de wereld van programmeren en softwareontwikkeling. Je zult zowel web- als native applicaties leren bouwen met talen zoals HTML, CSS, JavaScript, C# en Python. In vier jaar tijd transformeer je van een beginner naar een bekwame softwareontwikkelaar, klaar om te werken in een breed scala van ICT-gerelateerde bedrijven.</p>
+</div>
+<div class="bottom-box">
+    <div>
+        <h1>Blokken tot einde van de opleiding.</h1>
+        <img src="opleidingsoverzicht3.png" alt="Blokken">
     </div>
-        <div class="bottom-box">
-            <div>
-                <h1>Blokken tot einde van de opleiding.</h1>
-                <img src="opleidingsoverzicht3.png" alt="Blokken">
-            </div>
-            <div>
-                <h1>Voorbeeld rooster van het 1ste schooljaar.</h1>
-                <img src="rooster.png" alt="Rooster">
-            </div>
-        </div>
+    <div>
+        <h1>Voorbeeld rooster van het 1ste schooljaar.</h1>
+        <img src="rooster.png" alt="Rooster">
     </div>
+</div>
 
-    <?php    
+<?php    
 require_once("footer.php")
 ?>
+
+<script>
+    // Selecteer alle afbeeldingen binnen de bottom-box
+    const afbeeldingen = document.querySelectorAll('.bottom-box img');
+
+    // Voeg een click-event toe aan elke afbeelding
+    afbeeldingen.forEach(function(afbeelding) {
+        afbeelding.addEventListener('click', function() {
+            // Toggle de fullscreen class
+            this.classList.toggle('fullscreen');
+        });
+    });
+</script>
 </body>
 </html>
